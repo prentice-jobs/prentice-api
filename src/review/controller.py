fc26a681c6e6217d8157f04da3e0d3847d92d557
jeremyalv <jeremyalvax@gmail.com>
2024-06-08 20:50:13 +0700

    fix: import errors

--- src/review/controller.py
+++ src/review/controller.py
@@ -1,12 +1,15 @@
 from http import HTTPStatus
 from typing_extensions import Annotated
-
+from sqlalchemy.orm import Session
 from fastapi import (
     APIRouter,
     Request,
     Response,
     Depends,
     Body,
+
+    File,
+    UploadFile,
 )
 
 from fastapi.responses import (
@@ -14,12 +17,19 @@ from fastapi.responses import (
 )
 
 from fastapi.encoders import jsonable_encoder
+from src.core.schema import GenericAPIResponseModel
+
+from src.account.model import User
+from src.account.security import get_current_user
 
 from src.utils.db import get_db
-from src.core.schema import GenericAPIResponseModel
 from src.utils.response_builder import build_api_response
 
-from src.review.service import ReviewService
+from src.review.services.review_service import ReviewService
+from src.review.schema import (
+    CompanyReviewModelSchema,
+    CreateCompanyReviewSchema,
+)
 
 # TODO delete and adjust with ML model response
 from src.review.constants.temporary import (
@@ -42,3 +52,16 @@ def fetch_user_feed(
     
     return build_api_response(response)
 
+@review_router.post("/", status_code=HTTPStatus.CREATED, response_model=GenericAPIResponseModel)
+def create_new_review(
+    payload: CreateCompanyReviewSchema = Body(),
+    session: Session = Depends(get_db),
+):
+    pass
+
+@review_router.post("/upload/offer-letter", status_code=HTTPStatus.OK, response_class=GenericAPIResponseModel)
+def upload_offer_letter(
+    user: User = Depends(get_current_user),
+    offer_letter_file: UploadFile = File(...)
+):
+    pass
--- src/review/schema.py
+++ src/review/schema.py
@@ -16,16 +16,15 @@ from src.core.schema import (
     PrenticeBaseSchema
 )
 
-class CompanyReviewModelSchema(PrenticeBaseSchema):
-    """Schema for creating CompanyModel SQLAlchemy Objects"""
+class CreateCompanyReviewSchema(BaseModel):
+    """Schema for user's input when creating new Company Review"""
     company_id: UUID4
-    author_id: UUID4
     location: Annotated[str, Field(min_length=1, max_length=200)]
     is_remote: bool
 
     tags: List[Annotated[str, Field(min_length=1, max_length=500)]]
     star_rating: Annotated[float, Field(strict=True, le=5, ge=0)] # Float from 0.0 - 5.0
-    
+
     title: Annotated[str, Field(min_length=1, max_length=255)]
     description: Annotated[str, Field(min_length=0, max_length=5000)]
     role: Annotated[str, Field(min_length=1, max_length=255)]
@@ -37,3 +36,11 @@ class CompanyReviewModelSchema(PrenticeBaseSchema):
     annual_salary: int # 64-bit Python-primitive integer type
     salary_currency: Annotated[str, Field(min_length=1, max_length=3)] # ISO 4217 3-letter currency code
     
+
+class CompanyReviewModelSchema(CreateCompanyReviewSchema, PrenticeBaseSchema):
+    """Schema for creating CompanyModel SQLAlchemy Objects"""
+    author_id: UUID4
+
+    offer_letter_url: str # URL to object storage for uploaded Offer Letter
+    annual_salary: int # 64-bit Python-primitive integer type
+    salary_currency: Annotated[str, Field(min_length=1, max_length=3)] # ISO 4217 3-letter currency code
