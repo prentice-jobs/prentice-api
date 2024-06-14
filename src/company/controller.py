08809a5aabf8e58db4bdec5eee30de5784343f40
risalstr <risa.lestari2002@gmail.com>
2024-06-14 23:26:41 +0700

    fix:fixing logic bugs in salary and company

--- src/company/controller.py
+++ src/company/controller.py
@@ -4,43 +4,66 @@ from fastapi import (
     Depends,
     Body,
     HTTPException,
+    Query,
 )
 from sqlalchemy.orm import Session
 from uuid import UUID
 from fastapi.encoders import jsonable_encoder
 
 from src.company.models import Companies
-from src.company.schema import CompanyCreate, CompanyUpdate
+from src.company.schema import CompanyCreate, CompanyName, CompanyUpdate
 from src.company.service import CompanyService
+from src.core.schema import GenericAPIResponseModel
 from src.company import service, schema
 from src.utils.db import get_db
 
 VERSION = "v1"
 ENDPOINT = "company"
 
-company_router = APIRouter(
-    prefix=f"/{VERSION}/{ENDPOINT}",
-    tags=[ENDPOINT]
+company_router = APIRouter(prefix=f"/{VERSION}/{ENDPOINT}", tags=[ENDPOINT])
+
+@company_router.get(
+    "/all",
+    response_description="Fetch all the companies",
+    status_code=http.HTTPStatus.OK,
 )
-
-
-@company_router.get("/all",
-                    response_description="Fetch all the companies",
-                    status_code=http.HTTPStatus.OK,
-                    )
 def fetch_all_companies(db: Session = Depends(get_db)):
     service = CompanyService()
     companies = service.get_all_companies(db)
     return companies
 
+@company_router.get(
+    "/search-name",
+    response_description="Search companies by name",
+    status_code=http.HTTPStatus.OK,
+)
+def search_company_by_name(name: str = Query(..., description="Name of the company to search for"), db: Session = Depends(get_db)):
+    service = CompanyService()
+    companies = service.search_companies_by_name(db, name=name)
+    return companies
 
-@company_router.get("/{company_id}",
-                    response_description="Fetch a specific company",
-                    status_code=http.HTTPStatus.OK,
-                    )
+@company_router.get(
+    "/search-tags",
+    response_description="Search companies by name or tags",
+    status_code=http.HTTPStatus.OK,
+)
+def search_company(
+    name: str = Query(None, description="Name of the company to search for"),
+    tags: str = Query(None, description="Comma-separated tags to filter companies"),
+    db: Session = Depends(get_db)
+):
+    service = CompanyService()
+    companies = service.search_companies_by_tags(db, name=name, tags=tags)
+    return companies
+
+@company_router.get(
+    "/{company_id}",
+    response_description="Fetch a specific company",
+    status_code=http.HTTPStatus.OK,
+)
 def fetch_company_by_id(company_id: UUID, db: Session = Depends(get_db)):
     service = CompanyService()
-    db_company = service.get_company_by_id(db, company_id=company_id)
+    db_company = service.get_company_by_id(db=db, company_id=company_id)
     if db_company is None:
         raise HTTPException(status_code=404, detail="Company not found")
     return db_company
@@ -62,7 +85,7 @@ def create_company(db: Session = Depends(get_db), payload: CompanyCreate = Body(
         description=payload.description,
         tags=payload.tags,
         review_count=payload.review_count,
-        company_review=payload.company_review
+        company_review=payload.company_review,
     )
 
     payload_json = jsonable_encoder(req_company)
@@ -71,23 +94,26 @@ def create_company(db: Session = Depends(get_db), payload: CompanyCreate = Body(
     return company
 
 
-@company_router.patch("/{company_id}",
-                      status_code=http.HTTPStatus.OK,
-                      )
-def update_company(company_id: str, company: CompanyUpdate, db: Session = Depends(get_db)):
+@company_router.patch(
+    "/{company_id}",
+    status_code=http.HTTPStatus.OK,
+)
+def update_company(
+    company_id: str, company: CompanyUpdate, db: Session = Depends(get_db)
+):
     service = CompanyService()
 
-    db_company = service.update_company(
-        db, company_id=company_id, company=company)
+    db_company = service.update_company(db, company_id=company_id, company=company)
     if db_company is None:
         raise HTTPException(status_code=404, detail="Company not found")
     return db_company
 
 
-@company_router.delete("/{company_id}",
-                       status_code=http.HTTPStatus.OK,
-                       )
-def delete_companies_by_id(company_id: UUID, db: Session = Depends(get_db)):
+@company_router.delete(
+    "/{company_id}",
+    status_code=http.HTTPStatus.OK,
+)
+def delete_company_by_id(company_id: UUID, db: Session = Depends(get_db)):
     service = CompanyService()
     db_company = service.delete_company(db, company_id=company_id)
     if db_company is None:
--- src/company/service.py
+++ src/company/service.py
@@ -1,17 +1,75 @@
 from sqlalchemy.orm import Session
 from uuid import UUID
+from http import HTTPStatus
 
 from src.company.models import Companies
 from src.company.schema import CompanyCreate, CompanyUpdate, CompanyJSONRequestSchema
+from src.salary.services.get_all_reviews_service import ReviewService
+from src.core.schema import GenericAPIResponseModel
 
 
 class CompanyService:
+
+    def find_company_by_id(self, db: Session, company_id: UUID):
+        service = ReviewService()
+        company_reviews = []
+        
+        for review in service.get_all_reviews(db=db):
+            if str(review.get("company_id")) == str(company_id):
+                company_reviews.append({
+                    "title": review.get("title"),
+                    "description": review.get("description")
+                })
+        
+        data = db.query(Companies).filter(Companies.id == company_id).first()
+        
+        if not data:
+            raise HTTPException(status_code=404, detail="Company not found")
+
+        response = {
+            "status": HTTPStatus.OK,
+            "message": "success",
+            "id": data.id,
+            "created_at": data.created_at,
+            "updated_at": data.updated_at,
+            "is_deleted": data.is_deleted,
+            "display_name": data.display_name,
+            "logo_url": data.logo_url,
+            "company_sentiment": data.company_sentiment,
+            "review_count": len(company_reviews),
+            "description": data.description,
+            "star_rating": data.star_rating,  # Make sure to get the star rating from data, not review
+            "tags": data.tags,
+            "company_reviews": company_reviews,
+        }
+  
+        return response
+
     def get_company_by_id(self, db: Session, company_id: UUID):
-        return db.query(Companies).filter(Companies.id == company_id).first()
+        return self.find_company_by_id(db=db, company_id=company_id)
 
     def get_all_companies(self, db: Session):
         get_all = db.query(Companies).all()
-        return get_all or None
+        return get_all or []
+
+    def search_companies_by_name(self, db: Session, name: str):
+        return db.query(Companies).filter(Companies.display_name.ilike(f"%{name}%")).all()
+
+    def search_companies_by_tags(self, db: Session, name: str = None, tags: str = None):
+        query = db.query(Companies)
+        
+        if name:
+            query = query.filter(Companies.display_name.ilike(f"%{name}%"))
+        
+        if tags:
+            tag_list = tags.split(',')
+            query = query.filter(Companies.tags.overlap(tag_list))
+        
+        return query.all()
+
+    def get_company_id_by_name(self, db: Session, company_name: str):
+        company = db.query(Companies).filter(Companies.display_name == company_name).first()
+        return company.id if company else None
 
     def create_company(self, db: Session, payload: dict):
         company = Companies(**payload)
@@ -21,19 +79,21 @@ class CompanyService:
         return company
 
     def update_company(self, db: Session, company_id: UUID, company: CompanyUpdate):
-        db_company = db.query(Companies).filter(
-            Companies.id == company_id).first()
+        db_company = db.query(Companies).filter(Companies.id == company_id).first()
         if db_company:
             for key, value in company.dict(exclude_unset=True).items():
                 setattr(db_company, key, value)
             db.commit()
             db.refresh(db_company)
+        else:
+            raise HTTPException(status_code=404, detail="Company not found")
         return db_company
 
     def delete_company(self, db: Session, company_id: UUID):
-        db_company = db.query(Companies).filter(
-            Companies.id == company_id).first()
+        db_company = db.query(Companies).filter(Companies.id == company_id).first()
         if db_company:
             db.delete(db_company)
             db.commit()
+        else:
+            raise HTTPException(status_code=404, detail="Company not found")
         return db_company
