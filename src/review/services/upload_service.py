6c6db90e942b6be682102c2191b947410252aca3
jeremyalv <jeremyalvax@gmail.com>
2024-06-15 22:38:29 +0700

    feat: add fetch stopwords from gcs

--- src/review/services/upload_service.py
+++ src/review/services/upload_service.py
@@ -11,23 +11,25 @@ from src.core.schema import GenericAPIResponseModel
 from src.review.constants import messages as ReviewMessages
 
 from src.utils.settings import (
-    GCS_BUCKET_PRENTICE
+    GCS_BUCKET_OFFER_LETTER,
+    GCS_BUCKET_STOPWORDS,
 )
 
 class UploadService:
     def __init__(self) -> None:
         self.client = storage.Client()
-        self.bucket_name = GCS_BUCKET_PRENTICE
+        self.offer_letter_bucket = GCS_BUCKET_OFFER_LETTER
+        self.stopwords_bucket = GCS_BUCKET_STOPWORDS
         
     def upload_file(self, file: UploadFile, user_id: UUID4) -> GenericAPIResponseModel:
         try:
-            bucket = self.client.get_bucket(self.bucket_name)
+            bucket = self.client.get_bucket(self.offer_letter_bucket)
         
             file_path = self.construct_file_path(user_id=user_id, file_name=file.filename)
             blob =  bucket.blob(file_path)
             blob.upload_from_file(file.file, content_type="image/jpeg")
 
-            url = f"https://storage.cloud.google.com/{self.bucket_name}/{file_path}"
+            url = f"https://storage.cloud.google.com/{self.offer_letter_bucket}/{file_path}"
             
             response = GenericAPIResponseModel(
                 status=HTTPStatus.CREATED,
@@ -46,6 +48,22 @@ class UploadService:
             )
 
             return response
+        
+    def fetch_stopwords_array(self):
+        blob_name = "stopwords.txt"
+
+        bucket = self.client.get_bucket(self.stopwords_bucket)
+        blob = bucket.blob(blob_name)
+        
+        stopwords_array = []
+    
+        with blob.open("r") as f:
+            for line in f:
+                stopwords_array.append(line.strip())
+        
+        return stopwords_array
+
+
 
     def construct_file_path(self, user_id: UUID4, file_name: str) -> str:
         return f"{str(user_id)}/{file_name}"
